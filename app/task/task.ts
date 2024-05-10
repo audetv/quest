50b965d72268ac2155c0f491f3614b0413462b3f
audetv <algsound@yandex.ru>
2024-05-10 18:26:00 +0300

    Добавлен title в модель задача

--- app/task/task.ts
+++ app/task/task.ts
@@ -1,5 +1,9 @@
 namespace $ {
-	export class audetv_quest_task extends $mol_object {
-		
+	export class audetv_quest_task extends $hyoo_crowd_struct {
+
+		@$mol_mem
+		title( next?: string ) {
+			return this.sub( 'title', $hyoo_crowd_reg ).str( next )
+		}
 	}
 }
