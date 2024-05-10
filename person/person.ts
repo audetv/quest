c47799b982903fdc2d22f67e5eda031094aba3f7
audetv <algsound@yandex.ru>
2024-05-10 22:37:29 +0300

    added person space actions

--- person/person.ts
+++ person/person.ts
@@ -12,6 +12,41 @@ namespace $ {
 		// 	return this.sub( 'avatar', $hyoo_crowd_reg ).str( next )
 		// }
 
+		@$mol_mem
+		spaces_node() {
+			return this.sub( 'spaces', $hyoo_crowd_list )
+		}
+
+		@$mol_mem
+		space_fund() {
+			return this.world()!.Fund( $audetv_quest_space )
+		}
+
+		// список spaces
+		@$mol_mem
+		spaces_list() {
+			// достаем список айдишников
+			const ids = this.spaces_node().list()
+			// проверяем, что айдишники, что не айдишники отсекаем
+			// и из оставшихся айдишников создаем инстансы класса space
+			return ids
+				.map( id => $mol_int62_string_ensure( id ) )
+				.filter( $mol_guard_defined )
+				.map( id => this.space_fund()!.Item( id ) )
+		}
+
+		// добавляем space
+		@$mol_action
+		space_add() {
+			return this.space_fund().make()
+		}
+
+		// удаляем space
+		@$mol_action
+		space_drop( obj: $audetv_quest_space) {
+			this.spaces_node().drop( obj.head )
+		}
+
 	}
 
 }
